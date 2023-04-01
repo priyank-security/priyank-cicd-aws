e172e1e942906f6cc83581f5b0ec41702f836af6
Priyank Pathak <Priyank.Pathak@fidelity.co.in>
2023-04-01 18:10:43 +0530

    added s3 code

--- main.tf
+++ main.tf
@@ -5,3 +5,4 @@ provider "aws" {
   access_key = "AKIA3NA6QX5YXMORLOHB"
   secret_key = "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"
 }
+
---
+++ s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "my-first-tf-bucket" {
+  bucket = "my-tf-test-bucket"
+}
