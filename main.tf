16db2a116d9eccdf2d6a881f416484920f464397
Priyank Pathak <Priyank.Pathak@fidelity.co.in>
2023-04-01 17:39:40 +0530

    file modified

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
 terraform {
   backend "s3" {
-    bucket     = "project-aws-cicd-pipeline"
-    encrypt    = true
-    key        = "terraform.tfstate"
-    region     = "us-east-1"
-    access_key = "AKIA3NA6QX5YY27PXL7X"
-    secret_key = "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"
+    bucket  = "project-aws-cicd-pipeline"
+    encrypt = true
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
   }
 }
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA3NA6QX5YY27PXL7X"
-  secret_key = "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"
+  access_key = "AKIA3NA6QX5YTZIIPAM2"
+  secret_key = "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"
 }
